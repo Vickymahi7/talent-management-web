0255eb66a96e25d4a53349645a8eefb45afc9999
Vignesh <vickyruv@gmail.com>
2023-11-07 00:16:07 +0530

    Typescript implementation (Pending)

---
+++ src/types/Project.ts
@@ -0,0 +1,9 @@
+export interface Project {
+  title: string;
+  start_date: string;
+  end_date: string;
+  client: string;
+  technology: string;
+  description: string;
+  location: string;
+}
---
+++ src/types/WorkExperience.ts
@@ -0,0 +1,8 @@
+export interface WorkExperience {
+  company: string;
+  position: string;
+  location: string;
+  start_date: string;
+  end_date: string;
+  description: string;
+}
